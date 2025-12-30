2f7ca7cc3c9dbf1a056d99071dc25c5c68f6fb0e
aditi0319 <114223064+aditi0319@users.noreply.github.com>
2025-12-30 23:27:24 +0530

    updated link

--- client/my-react-app/src/services/api.js
+++ client/my-react-app/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://magnet-task-1-qh9j.onrender.com/api",
 });
+
 
 API.interceptors.request.use((req) => {
   const user = JSON.parse(localStorage.getItem("user"));
